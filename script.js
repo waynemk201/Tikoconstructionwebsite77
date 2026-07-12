af7b018341cb5480ec843f3b8b4bfa92d6acc513
waynemk201 <mukomberowayne0@gmail.com>
2026-07-12 21:44:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// ================================
+// NAVBAR SCROLL EFFECT
+// ================================
+const header = document.getElementById("header");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+// ================================
+// SMOOTH SCROLL
+// ================================
+const links = document.querySelectorAll('a[href^="#"]');
+links.forEach(link => {
+    link.addEventListener("click", function (e) {
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            e.preventDefault();
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
